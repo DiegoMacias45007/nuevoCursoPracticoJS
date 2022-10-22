874750a0835856d979c6d5298af8cf79c388593a
Diego Macias <djoromac@hotmail.com>
2022-10-22 01:00:25 -0500

    Agregamos unas cosas

---
+++ manipulacionDOM/js/index.js
@@ -0,0 +1,7 @@
+// query selector funciona para todas las etiquetas del HTML que hayas planteado, como si fuera css y se tienen que especificar con string
+
+const h1 =document.querySelector("h1");
+
+const parrafito = document.querySelector('.parrafito');
+
+const pid = document.querySelector('#pid');
